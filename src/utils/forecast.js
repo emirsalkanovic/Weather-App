434438a111fc678040a3af21372784070971efa4
Emir Salkanovic <emirsalkanovic1@gamil.com>
2019-04-06 11:09:16 +0200

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+	const url = ('https://api.darksky.net/forecast/96d548b9b4f8b3a5f01f94ec62ef50ae/' + latitude + ',' + longitude + '?units=si')
+
+	request({ url, json: true }, (error, { body }) => {
+		if (error) {
+			callback('Unable to connect to connect to Weather App service', undefined)
+		} else if (body.error) {
+			callback('Unable to find location. Please try again', undefined)
+		} else {
+			callback(undefined, body.daily.data[0].summary + ' Current temperature is : ' + body.currently.temperature + '. There is ' + body.currently.precipProbability + '% chance of rain.')
+		}
+	})
+}
+
+module.exports = forecast;
